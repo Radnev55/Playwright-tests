4bf22de1c716721185c8eb1f8d5eb329d728483e
Martin <martinradnev55@gmail.com>
2025-08-08 14:45:41 +0300

    Clean old tests

--- tests/home.spec.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Open homepage and check title', async ({ page }) => {
-  await page.goto('/angularpractice/');
-  await expect(page).toHaveTitle('ProtoCommerce');
-});
